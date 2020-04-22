ea0add44272d78a6393f27f4e5efe751c49c511d
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-04-22 07:59:08 -0300

    Exercicios!

--- .vscode/python_+100exercicios/ex001.py
+++ .vscode/python_+100exercicios/ex001.py
@@ -4,3 +4,17 @@ Crie um programa que escreva "Olá Mundo" na tela.
 
 msg = ("Olá, Mundo!")
 print(msg)
+
+nome=input("Olá, qual o seu nome? ")
+print("É um grande prazer te conhecer,",nome)
+
+email=input("Qual o seu e-mail ?")
+
+data_nascimento=input("Qual a data do seu nascimento ?")
+
+naturalidade=input("Onde você nasceu?")
+
+profissao=input("Qual a sua profissão ?")
+
+print("Seus dados :",nome, email, data_nascimento, naturalidade, profissao)
+
