d6aef280e287f27a3c9b0d6cdee34a4cd2470fb7
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-04-22 07:09:51 -0300

    Tipos primitivos de dados

---
+++ .vscode/python_+100exercicios/tiposprimitivos_saidadedados.py
@@ -0,0 +1,21 @@
+# Toda linguagem de progamação trabalha com 4 tipos primitivos de dados, python até possui mais tipos, porém só veremos 4 tipos.
+
+#A recomendação da comuniade é usar sempre aspas simples para strings
+#Ao 
+n1=int(input('Digite um número: '))
+n2=int(input('Digite outro número: '))
+# Neste caso ( s=n1+n2 ) O mais serve como concatenação.
+s=n1+n2
+print('O resultado é:',s)
+
+'''
+Tipos primitivos de dados:
+Números inteiros:
+    int = 7, -4, 0, 1654169
+Números reais ou com ponto flutuante
+    float= 4.5, 0.75, -3.14, 7.0
+Valores lógicos ou booleanos: Sendo a primeira letra da palavra sempre maiuscula.
+    bool= True, False
+Caractéres ou strings:
+    str = 'Olá', '7.5', ''
+'''
