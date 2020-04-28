32298498d30ce21287c102ec70538c59f2e02951
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-04-28 02:34:00 -0300

    Aula 07 operadores artiméticos

---
+++ .vscode/python_+100exercicios.py/aula07a.py
@@ -0,0 +1,55 @@
+'''Utilizando os operadores aritmeticos na prática. 
+
+No console:
+>>>5+3*2 #Primeiro faz a multiplicação e depois a soma
+11
+
+>>> 5**2 #Valor ao quadrado
+25
+
+>>> 5**3 #valor ao cubo
+125
+
+>>> 19//2 #Divisão inteira
+9
+
+>>> 19/2 #Divisão real
+
+>>> 365**522 #Em python o limite é o tamanho da memória.
+32875717981981342289734219608076461210798383174338902362440525567069083206084822182430672
+...
+
+>>> 18%2 #Resto da divisão
+0
+
+>>> 122%3
+2
+
+>>> 4**3 #Ao cubo
+64
+>>> pow(4,3) # Usando função
+64
+
+>>> 1/2 #Raiz quadrada de um valor
+0.5
+>>> 81**(1/2) #Faz a divisão de 1/2 = 0.5, pega o valor e eleva a 0.5
+9.0
+>>> 25**(1/2)
+5.0
+
+>>> 127**(1/3) #Raiz cúbica
+5.026525695313479
+
+>>> 'Oi' + 'Ola' # Concatena
+'OiOla'
+
+>>> 'Oi' * 5 #Multiplica o conjunto de strings
+'OiOiOiOiOi'
+
+>>> '='*20
+'===================='
+
+>>> print('='*20)
+====================
+'''
+
