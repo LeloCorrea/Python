6d1dddb7d92c252d380348e42de2193bc5e58565
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-04-28 01:03:27 -0300

    Atributos e métodos em variáveis str.

--- .vscode/python_+100exercicios.py/ex004.py
+++ .vscode/python_+100exercicios.py/ex004.py
@@ -14,3 +14,8 @@ print('Esta em capitalizada?', algo.istitle())
 
 
 #A função input retorna sempre uma string.
+
+'''
+Neste caso específico a variável algo é um objeto.
+Todo o objeto tem características e realiza funcionalidades, então eles tem atributos e métodos. Como tem parenteses depois de cada um deles, estamos trabalhando métodos. Toda o objeto string tem esses métodos: islower, isupper, isnumeric, istitle...
+'''
