64b0165605aff86adc432c1dfb7de45824064cfa
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-04-28 23:58:01 -0300

    Desafio 10 (Comprando dólares com a moeda reais)

--- .vscode/python_+100exercicios.py/desafio_10.py
+++ .vscode/python_+100exercicios.py/desafio_10.py
@@ -1,4 +1,8 @@
 '''Desafio 10 :
 Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
-Considerando o dolar  = 3.27.
+Considerando o dolar  = 5.50 (28/04/2020).
 '''
+dinheiro = float(input('Digite quanto dinheiro você tem na carteira: '))
+dollares = dinheiro / 5.50
+#Como estou usando float quero ver apenas duas casas depois da virgula com o {:.2f}.
+print('Com este dinheiro: R${:.2f}, você consegue comprar ${:.2f} dólares no dia de hoje (28/04/2020).'.format(dinheiro, dollares))
