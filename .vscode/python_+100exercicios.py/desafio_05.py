06b3b1d046bdc88c2d825a735a218b5a75091df5
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-04-28 22:34:27 -0300

    Desafio 5 (Mostra numero, sucessor e antecessor)

--- .vscode/python_+100exercicios.py/desafio_05.py
+++ .vscode/python_+100exercicios.py/desafio_05.py
@@ -1,2 +1,7 @@
 '''Desafio 5 :
 Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.'''
+
+num = int(input('Digite um número inteiro: '))
+sucessor = num +1
+antecessor = num - 1
+print('O número que você digitou é: {}, o seu sucessor é: {}, e o seu antecessor é: {}'.format(num, sucessor, antecessor))
