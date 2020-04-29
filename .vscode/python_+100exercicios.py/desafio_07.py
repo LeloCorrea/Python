3b14e8137014ca8aa96dd90db2f9802d5aea25be
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-04-28 23:19:22 -0300

    Desafio 08 (Recebe um valor em metros, mostra centimetros e milimetros)

--- .vscode/python_+100exercicios.py/desafio_07.py
+++ .vscode/python_+100exercicios.py/desafio_07.py
@@ -1,3 +1,8 @@
 '''Desafio 7 :
 Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 '''
+nome = str(input('Digite o nome do aluno: '))
+nota1 = int(input('Digite a primeira nota: '))
+nota2 = int(input('Digite a segunda nota: '))
+media = ((nota1 + nota2) / 2)
+print('O aluno: {}, tem a seguinte média: {}.\n(Pois a primeira nota foi: {} e a segunda nota foi:{} ).'.format(nome,media,nota1,nota2))
--- .vscode/python_+100exercicios.py/desafio_08.py
+++ .vscode/python_+100exercicios.py/desafio_08.py
@@ -1,3 +1,8 @@
 '''Desafio 8 :
 Escreva um programa que leia um valor em metros e exiba ele convertido em centímetros e milímetros.
 '''
+
+metros = float(input('Digite um valor em metros: '))
+centimetros = metros * 100
+milimetros = metros * 1000
+print('O valor que você digitou foi: {} metros que possuem: {} centimetros e {} milimetros.'.format(metros, centimetros, milimetros))
