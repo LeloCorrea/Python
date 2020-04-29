5d17317b76ada3dc800f222eb12bbce155d89c62
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-04-28 23:47:51 -0300

    Desafio 09 (tabuada)

--- .vscode/python_+100exercicios.py/desafio_09.py
+++ .vscode/python_+100exercicios.py/desafio_09.py
@@ -1,3 +1,7 @@
 '''Desafio 9 :
 Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
+Obs.: Não usei o for para executar a tabuada deste número, por neste curso que estou vendo ainda não foi ensinado como é feito o for em python.
 '''
+numero = int(input('Digite um valor: '))
+print('A tabuada do número: {} é:\n{} * 1 = {}\n{} * 2 = {}\n{} * 3 = {}\n{} * 4 = {}\n{} * 5 = {}\n{} * 6 = {}\n{} * 7 = {}\n{} * 8 = {}\n{} * 9 = {}\n{} * 10 = {}'.format(numero, numero, numero * 1, numero, numero * 2, numero, numero * 3, numero, numero * 4, numero, numero * 5, numero, numero * 6, numero, numero * 7, numero, numero * 8, numero, numero * 9, numero, numero * 10))
