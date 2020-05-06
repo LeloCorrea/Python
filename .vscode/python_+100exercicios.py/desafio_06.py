9ce758e9bcc071925aa68d95440bcb119f2a8916
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-05-06 01:18:36 -0300

    Desafio 06

--- .vscode/python_+100exercicios.py/desafio_06.py
+++ .vscode/python_+100exercicios.py/desafio_06.py
@@ -1,9 +1,15 @@
 '''Desafio 6 :
 Crie um algoritmo que leia um número e mostre o seu dobro, triplo e a raiz quadrada:
 '''
-
+'''
+#Primeira maneira de fazer
 num = int(input('Digite um número: '))
 dobro = num*2
 triplo = num*3
-raiz = num**(1/2)
-print('O número que você digitou é: {}, o seu dobro é: {}, o seu triplo é: {} e a sua raiz quadrada é: {:.2f}'.format(num, dobro, triplo, raiz))
+raiz = num**(1/2) #Raiz representada através de exponenciação (num elevado a meio)
+print('O número que você digitou é: {}\nO seu dobro é: {}\nO seu triplo é: {}\nA sua raiz quadrada é: {:.2f}.'.format(num, dobro, triplo, raiz))
+'''
+
+#Outra maneira de fazer
+n = int(input('Digite um número: '))
+print('O número que você digitou é: {}\nO seu dobro é: {}\nO seu triplo é: {}\nSua raiz é: {:.2f}.'.format(n, n*2, n*3, pow(n,1/2))) #Utilizando a função pow.
