77937609cc693e997ef0bd8ccc69fe686eea8c46
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-05-06 00:58:01 -0300

    Desafio 06 (dobro, triplo, raiz)

--- .vscode/python_+100exercicios.py/desafio_06.py
+++ .vscode/python_+100exercicios.py/desafio_06.py
@@ -6,4 +6,4 @@ num = int(input('Digite um número: '))
 dobro = num*2
 triplo = num*3
 raiz = num**(1/2)
-print('O número que você digitou é: {}, o seu dobro é: {}, o seu triplo é: {} e a sua raiz quadrada é: {}'.format(num, dobro, triplo, raiz))
+print('O número que você digitou é: {}, o seu dobro é: {}, o seu triplo é: {} e a sua raiz quadrada é: {:.2f}'.format(num, dobro, triplo, raiz))
