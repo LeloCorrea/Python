27c1f948a8fb01776f4570b71ee28824ef39ca24
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-05-06 02:41:01 -0300

    Desafio 8 mostrando cm e mm de uma medida

--- .vscode/python_+100exercicios.py/desafio_08.py
+++ .vscode/python_+100exercicios.py/desafio_08.py
@@ -2,8 +2,8 @@
 Escreva um programa que leia um valor em metros e exiba ele convertido em centímetros e milímetros.
 '''
 
-metros = float(input('Digite um valor em metros: '))
-centimetros = int(metros * 100)
-milimetros = int(metros * 1000)
+medida = float(input('Digite um valor em metros: '))
+centimetros = medida * 100
+milimetros = medida * 1000
 #Para mostrar valores com ponto flutuante é preciso usar {:.2f}
-print('O valor que você digitou foi: {:.2f} metros que possuem: {} centimetros e {} milimetros.'.format(metros, centimetros, milimetros))
+print('A medida de{}m corresponde a {:.0f}cm e {:.0f}mm.'.format(medida, centimetros, milimetros))
