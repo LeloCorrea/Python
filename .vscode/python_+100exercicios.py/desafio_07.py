775c9b255eb55146585b2768e6dde6902931fbe8
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-05-06 02:32:37 -0300

    Desafio 7 media de notas

--- .vscode/python_+100exercicios.py/desafio_07.py
+++ .vscode/python_+100exercicios.py/desafio_07.py
@@ -2,7 +2,7 @@
 Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 '''
 nome = str(input('Digite o nome do aluno: '))
-nota1 = int(input('Digite a primeira nota: '))
-nota2 = int(input('Digite a segunda nota: '))
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
 media = ((nota1 + nota2) / 2)
-print('O aluno: {}, tem a seguinte média: {}.\n(Pois a primeira nota foi: {} e a segunda nota foi:{} ).'.format(nome,media,nota1,nota2))
+print('O aluno: {}, tem a seguinte média: {:.1f}\n(Pois a primeira nota foi: {:.1f} e a segunda nota foi: {:.1f})'.format(nome,media,nota1,nota2))
