b31d98e7013d2a5aae140de4959f187bfbb3f87b
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-05-07 23:05:53 -0300

    Ex010

--- .vscode/python_+100exercicios.py/desafio_10.py
+++ .vscode/python_+100exercicios.py/desafio_10.py
@@ -1,8 +1,8 @@
 '''Desafio 10 :
 Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
-Considerando o dolar  = 5.50 (28/04/2020).
+Considerando 1 dolar  = 5.50 (28/04/2020).
 '''
-dinheiro = float(input('Digite quanto dinheiro você tem na carteira: '))
-dollares = dinheiro / 5.50
+real = float(input('Digite quanto dinheiro você tem na carteira: R$'))
+dollares = real / 5.50
 #Como estou usando float quero ver apenas duas casas depois da virgula com o {:.2f}.
-print('Com este dinheiro: R${:.2f}, você consegue comprar ${:.2f} dólares no dia de hoje (28/04/2020).'.format(dinheiro, dollares))
+print('Com este dinheiro: R${:.2f}, você consegue comprar ${:.2f} no dia de hoje (28/04/2020).'.format(real, dollares))
