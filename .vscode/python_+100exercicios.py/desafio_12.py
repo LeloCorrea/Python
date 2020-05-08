de042e710c5edbff0d9b12065f22e8da0467d020
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-05-08 00:41:54 -0300

    Desafio 11

--- .vscode/python_+100exercicios.py/desafio_12.py
+++ .vscode/python_+100exercicios.py/desafio_12.py
@@ -1,8 +1,8 @@
 '''Desafio 12 :
 Faça um algoritmo que leia o preço de um produto e mostre o seu novo preço, com 5% de desconto.
 '''
-produto = float(input('Digite o preço de um produto: R$'))
+preço = float(input('Digite o preço de um produto: R$'))
 desconto = 5
-economia = ((produto * desconto)/100)
-valorreal = produto - economia
-print('O produto tem o valor de: R$ {:.2f}, com 5% de desconto fica: R${:.2f}.\nVocê teve uma economia de: R${:.2f}.'.format(produto, valorreal, economia))
+economia = ((preço * desconto)/100)
+valorreal = preço - economia
+print('O produto tem o valor de: R$ {:.2f}, com 5% de desconto fica: R${:.2f}.\nVocê teve uma economia de: R${:.2f}.'.format(preço, valorreal, economia))
