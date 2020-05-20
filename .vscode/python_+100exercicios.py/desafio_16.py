a30496c238bed527b69d672820ca2e995a6a871c
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-05-20 00:09:20 -0300

    Desafio 16

--- .vscode/python_+100exercicios.py/desafio_16.py
+++ .vscode/python_+100exercicios.py/desafio_16.py
@@ -4,3 +4,20 @@ Crie um programa que leia um número real qualquer pelo teclado e mostre na tela
 Ex: Digite um número 6.127.
 O número 6.127 tem a parte inteira 6.
 '''
+'''
+#Primeira maneira:
+#import math
+from math import trunc
+numero = float(input('Digite um número: '))
+print('O valor digitado é: {} e a sua porção inteira é: {}.'.format(numero, math.trunc(numero)))
+'''
+'''
+#Segunda maneira:
+from math import trunc
+numero = float(input('Digite um número: '))
+print('O valor digitado é: {} e a sua porção inteira é: {}.'.format(numero, trunc(numero)))
+'''
+
+#Terceira maneira:
+num = float(input('Digite um valor: '))
+print('O valor digitado é: {} e a sua porção inteira é: {}.'.format(numero,int(num)))
