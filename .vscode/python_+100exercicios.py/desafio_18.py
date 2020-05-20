074abd2a85677fa33b82670d9df2c5bd32d0e725
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-05-20 00:43:14 -0300

    desafio 18

--- .vscode/python_+100exercicios.py/desafio_18.py
+++ .vscode/python_+100exercicios.py/desafio_18.py
@@ -1,3 +1,23 @@
 '''Desafio 18:
 Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
 '''
+'''
+#Lembrando a medida vertical é o seno a horizontal é o cosceno e a tangente é o angulo.
+import math
+angulo = float(input('Digite o angulo que você deseja: '))
+seno = math.sin(math.radians(angulo))
+print('O angulo de {} tem o seno de {:.2f}'.format(angulo, seno))
+cosseno = math.cos(math.radians(angulo))
+print('O angulo de {} tem o cosseno de {:.2f}'.format(angulo,cosseno))
+tangente = math.tan(math.radians(angulo))
+print('O angulo de {} tem a tangente de {:.2f}'.format(angulo,tangente))
+'''
+
+from math import radians, sin, cos, tan
+angulo = float(input('Digite o angulo que você deseja: '))
+seno = sin(radians(angulo))
+print('O angulo de {} tem o seno de {:.2f}'.format(angulo, seno))
+cosseno = cos(radians(angulo))
+print('O angulo de {} tem o cosseno de {:.2f}'.format(angulo,cosseno))
+tangente = tan(radians(angulo))
+print('O angulo de {} tem a tangente de {:.2f}'.format(angulo,tangente))
