a89b5f564597ca8eb40950bc3c1e2e7b4d373f4f
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-05-20 00:29:05 -0300

    desafio 17

--- .vscode/python_+100exercicios.py/desafio_17.py
+++ .vscode/python_+100exercicios.py/desafio_17.py
@@ -3,3 +3,22 @@ Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente 
 
 O quadrado da hipotenusa é igual a soma dos quadrados dos catetos.
 '''
+'''
+#Maneira tradicional matematicamente falando
+catetoOposto = float(input('Digite o comprimento to cateto oposto: '))
+catetoAdjacente = float(input('Digite o comprimento do cateto adjacente: '))
+hipotenusa = (catetoOposto **2 + catetoAdjacente **2) ** (1/2)
+print('A hipotenusa vai medir: {:.2f}'.format(hipotenusa))
+'''
+'''
+import math
+catetoOposto = float(input('Digite o comprimento to cateto oposto: '))
+catetoAdjacente = float(input('Digite o comprimento do cateto adjacente: '))
+hipotenusa = math.hypot(catetoOposto,catetoAdjacente)
+print('A hipotenusa vai medir: {:.2f}'.format(hipotenusa))
+'''
+from math import hypot
+catetoOposto = float(input('Digite o comprimento to cateto oposto: '))
+catetoAdjacente = float(input('Digite o comprimento do cateto adjacente: '))
+hipotenusa = hypot(catetoOposto,catetoAdjacente)
+print('A hipotenusa vai medir: {:.2f}'.format(hipotenusa))
