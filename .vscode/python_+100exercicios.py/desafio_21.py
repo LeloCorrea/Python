cc19526d36d62f2ad648affd46cd3233d19e9159
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-05-20 01:23:45 -0300

    Desafio 21

--- .vscode/python_+100exercicios.py/desafio_21.py
+++ .vscode/python_+100exercicios.py/desafio_21.py
@@ -1,3 +1,8 @@
 '''Desafio 21:
 Faça um programa em python que abra e reproduza o audio de um arquivo MP3.
 '''
+import pygame
+pygame.init()
+pygame.mixer.music.load('cd_meme.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
